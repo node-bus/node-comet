a3d01047b14b5614fd43b7e98a75962bd4d733ba
Yusuf Simonson <simonson@gmail.com>
2010-06-24 21:19:31 -0400

    Added documentation to client code

--- src/comet.client.js
+++ src/comet.client.js
@@ -1,8 +1,13 @@
 ;(function() {
+    //Pattern for determing the protocol of a url
     var PROTOCOL_PATTERN = /^(http|ws|wss|https):\/\//;
     
-    if (!Function.prototype.bind) { // check if native implementation available
-        Function.prototype.bind = function(){ 
+    //Don't add the method to the function prototype if a native implementation
+    //already exists. Based on the prototype.js implementation.
+    if (!Function.prototype.bind) {
+        //Exposes a mechanism for binding a function's scope to a specified
+        //object
+        Function.prototype.bind = function() {
             var fn = this;
             var args = Array.prototype.slice.call(arguments);
             var object = args.shift();
@@ -13,11 +18,9 @@
         };
     }
     
+    //Comet implementation that uses websockets
     function WebSocketClient(url) {
-        this.onMessage = null;
-        this.onError = null;
-        this.errors = 0;
-        
+        //Change the URL from http://... to ws://... if necessary
         var protocolMatch = PROTOCOL_PATTERN.exec(url)[1];
         
         if(protocolMatch === "https") {
@@ -33,21 +36,24 @@
     
     WebSocketClient.prototype = {
         type: 'websocket',
-        onMessage: null,
-        onError: null,
+        onmessage: null,
+        onerror: null,
         errors: 0,
         url: null,
         queue: [],
         
+        //Creates a new websocket and adds event listeners
         _init: function() {
             this._socket = new WebSocket(this.url);
             this._socket.onmessage = this._handleMessage.bind(this);
             this._socket.onerror = this._handleError.bind(this);
         },
         
+        //Called when a message is received from the websocket
         _handleMessage: function(message) {
             var json = null;
             
+            //Try to parse the JSON and throw an error if it fails
             try {
                 json = JSON.parse(message.data);
             } catch(e) {
@@ -58,17 +64,20 @@
             
             if(json) {
                 if(json.type == 'message') {
+                    //Call the message event listener if it's a message
                     this.onmessage(json.payload);
                 } else if(json.type == 'error' && json.payload) {
+                    //Call the error event listener if it's an error
                     this.onerror(json.payload.errorName, json.payload.message);
                 } else {
+                    //The server-sent JSON does not follow expected standards
                     this.onerror('receiveBadServerJSON', 'Received bad JSON message from the server');
                 }
             }
         },
         
+        //Call the error listener, then try to reconnect
         _handleError: function(error) {
-            // handle the error, then try to reconnect.
             if(this.onerror) {
                 this.onerror('connect', 'Could not connect');
             }
@@ -77,10 +86,13 @@
             setTimeout(this._init, 1000 * this.errors * this.errors);
         },
         
+        //Sends a message to the server
         send: function(json) {
             var data = JSON.stringify(json);
             var self = this;
             
+            //Keep trying to send the message until we're connected to the
+            //server
             var lazySend = function() {
                 if(self._socket.readyState != self._socket.OPEN) {
                     setTimeout(lazySend, 1);
@@ -92,12 +104,15 @@
             lazySend();
         },
         
+        //Closes the connection
         close: function() {
             this._socket.close();
         }
     };
     
+    //Comet implementation that uses long polling
     function LongPollingClient(url) {
+        //If the user accidently sent a websocket url, change it back to http
         var protocolMatch = PROTOCOL_PATTERN.exec(url)[1];
         
         if(protocolMatch === "wss") {
@@ -116,25 +131,20 @@
         clientId: null,
         errors: 0,
         
+        //Sets up the long-polling mechanisms
         _init: function() {
-            // set up the long-polling mechanisms.
             var self = this;
             
+            //Connects to the server using xhr
             var connect = function() {
                 var socket = self._createXHR();
                 self._socket = socket;
                 
+                //Handles a response from the xhr object
                 socket.onreadystatechange = function() {
-                    // summary:
-                    //          Handles the response for the long-polling 
-                    //          request.
-                    
                     if(socket.readyState == 4) {
                         if(socket.status >= 200 && socket.status <= 299) {
-                            // Unlike for websockets, the longpolling responses
-                            // come back as an array. Parse it here.
-                            console.log('received' + socket.responseText);
-                            
+                            //Parse the response
                             var json = JSON.parse(socket.responseText);
                             var payload = json.payload;
                             
@@ -145,13 +155,13 @@
                                 }
                             }
                             
-                            // Grab the lastEventId.
+                            //Grab the client id
                             self.clientId = json.clientId;
                             
-                            // Reopen the connection immediately.
+                            //Reopen the connection immediately.
                             connect();
                         } else {
-                            // handle the error, then try to reconnect.
+                            //Handle the error, then try to reconnect.
                             if(self.onerror) self.onerror(socket);
                             self.errors++;
                             setTimeout(connect, 1000 * self.errors * self.errors);
@@ -177,33 +187,32 @@
             connect();
         },
         
+        //Sends a message to the server
         send: function(data) {
             var xhr = this._createXHR();
             
+            //Handles a response from the xhr object
             xhr.onReadyStateChange = function() {
                 if(xhr.readyState == 4 && (xhr.status < 200 || xhr.status > 299)) {
                     if(this.onerror) this.onerror(xhr);
                 }
             }
             
+            //Send the message
             xhr.open("POST", this.url, true);
             xhr.setRequestHeader("Content-type","application/json");
             xhr.send(JSON.stringify(data));
         },
         
+        //Close the socket
         close: function() {
             if(this._socket && this._socket.readyState != 4) {
                 this._socket.abort();
             }
         },
         
+        //Creates an XHR object for AJAX
         _createXHR: function() {
-            // summary:
-            //          creates an XHR object for AJAX...
-            // return:
-            //          In all normal browsers: XMLHttpRequest, 
-            //          IE6: ActiveXObject.
-            
             if(window.XMLHttpRequest) {
                 return new XMLHttpRequest();
             } else {
@@ -216,6 +225,7 @@
         }
     };
     
+    //Creates a comet connection based on the capabilities of the client
     function comet(url) {
         var cls = window.WebSocket ? WebSocketClient : LongPollingClient;
         return new cls(url);
